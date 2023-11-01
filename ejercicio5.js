cfe6efca7190498bdfbc78cf749b4370edfaa760
Cynthia Ortiz <cynortiz21@gmail.com>
2023-11-01 00:56:50 -0300

    resolvi ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt("Introduce el primer número:"));
+let num2 = parseInt(prompt("Introduce el segundo número:"));
+let resultado = num1 + num2;
+document.write("La suma de los dos números es: " + resultado);
